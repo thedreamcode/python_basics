abda3d05057836794fbee915bf741bbb0ca10177
thedreamcode <iomeglero@gmail.com>
2019-05-05 16:35:20 +0300

    Update on read_cam

--- read_camera.py
+++ read_camera.py
@@ -1,18 +1,18 @@
 import cv2
 
 
-def getVideoSource(source):
+def getVideoSource(source, width, height):
     cap = cv2.VideoCapture(source)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     return cap
 
 def main():
-    camera = getVideoSource(0)
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-    
-    while True:
-        ret, frame = camera.read()
+    camera = getVideoSource(0, 1080, 720)
 
+    while True:
+            
+        ret, frame = camera.read()
         cv2.imshow('Camera', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
